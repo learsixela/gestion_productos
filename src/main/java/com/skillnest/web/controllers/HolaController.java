376335cd7dc8f16a22cb1545789476023536fce4
Israel <ipalma@gmail.com>
2025-10-22 22:01:48 -0300

    Se agrega Logger

--- src/main/java/com/skillnest/web/controllers/HolaController.java
+++ src/main/java/com/skillnest/web/controllers/HolaController.java
@@ -1,15 +1,18 @@
 package com.skillnest.web.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.RestController;
 
 import com.skillnest.web.Component.MiServicio;
 
-//@Slf4j
-@Controller
+
+@RestController
 public class HolaController {
+	private static final Logger log = LoggerFactory.getLogger(HolaController.class);
 	//inyeccion de dependencias
 	@Autowired
 	MiServicio servicio;
@@ -17,6 +20,7 @@ public class HolaController {
 	//http://localhost:8080/mensaje
     @GetMapping("/mensaje")
     public String mensaje() {
+    	log.info("Accediendo al endpoint /mensaje para probar el logger");
         return servicio.obtenerMensaje();
     }
 
