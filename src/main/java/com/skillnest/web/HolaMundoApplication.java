fc32b60d6c9160c1f1f0d1a772bdc930572ed076
Israel <ipalma@gmail.com>
2025-10-23 20:02:52 -0300

    se genera el war

--- src/main/java/com/skillnest/web/HolaMundoApplication.java
+++ src/main/java/com/skillnest/web/HolaMundoApplication.java
@@ -2,6 +2,8 @@ package com.skillnest.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 //1. anotacion
 @RestController
-public class HolaMundoApplication {
+public class HolaMundoApplication extends SpringBootServletInitializer{
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(HolaMundoApplication.class);
+    }
+    
 	public static void main(String[] args) {
 		SpringApplication.run(HolaMundoApplication.class, args);
 	}
