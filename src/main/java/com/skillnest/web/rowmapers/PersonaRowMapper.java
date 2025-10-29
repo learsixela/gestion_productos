998f2332c7511e2cd013b6542052afddac601260
Israel <ipalma@gmail.com>
2025-10-28 21:26:21 -0300

    Cambio a PersonaDTO

--- src/main/java/com/skillnest/web/rowmapers/PersonaRowMapper.java
+++ src/main/java/com/skillnest/web/rowmapers/PersonaRowMapper.java
@@ -5,13 +5,13 @@ import java.sql.SQLException;
 
 import org.springframework.jdbc.core.RowMapper;
 
-import com.skillnest.web.models.Persona;
+import com.skillnest.web.models.PersonaDTO;
 
-public class PersonaRowMapper implements RowMapper<Persona> {
+public class PersonaRowMapper implements RowMapper<PersonaDTO> {
     
 	@Override
-    public Persona mapRow(ResultSet rs, int rowNum) throws SQLException {
-        Persona persona = new Persona();
+    public PersonaDTO mapRow(ResultSet rs, int rowNum) throws SQLException {
+		PersonaDTO persona = new PersonaDTO();
         persona.setId(rs.getInt("id"));
         persona.setNombre(rs.getString("nombre"));
         persona.setEmail(rs.getString("email"));
