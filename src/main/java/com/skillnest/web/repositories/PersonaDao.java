763659d51881ea6069fc2cb1e418dee0a9a13079
Israel <ipalma@gmail.com>
2025-10-29 19:51:09 -0300

    OneToOne and ManyToOne

--- src/main/java/com/skillnest/web/repositories/PersonaDao.java
+++ src/main/java/com/skillnest/web/repositories/PersonaDao.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
-import com.skillnest.web.models.Persona;
+import com.skillnest.web.models.PersonaDTO;
 import com.skillnest.web.rowmapers.PersonaRowMapper;
 
 @Repository
@@ -17,29 +17,29 @@ public class PersonaDao {
 
     // Métodos CRUD aquí
     
-    public List<Persona> obtenerTodas() {
+    public List<PersonaDTO> obtenerTodas() {
         String sql = "SELECT * FROM personas";
         return jdbcTemplate.query(sql, new PersonaRowMapper());
     }
 
-    public Persona obtenerPorId(Long id) {
+    public PersonaDTO obtenerPorId(Long id) {
         String sql = "SELECT * FROM personas WHERE id = ?";
         return jdbcTemplate.queryForObject(sql, new PersonaRowMapper(), id);
     }
     
-    public Persona obtenerPorEmail(String email) {
+    public PersonaDTO obtenerPorEmail(String email) {
         String sql = "SELECT * FROM personas WHERE email = ?";
         return jdbcTemplate.queryForObject(sql, new PersonaRowMapper(), email);
     }
     
  // INSERT
-    public int insertar(Persona persona) {
+    public int insertar(PersonaDTO persona) {
         String sql = "INSERT INTO personas (nombre, email) VALUES (?, ?)";
         return jdbcTemplate.update(sql, persona.getNombre(), persona.getEmail());
     }
 
     // UPDATE
-    public int actualizar(Persona persona) {
+    public int actualizar(PersonaDTO persona) {
         String sql = "UPDATE personas SET nombre = ?, email = ? WHERE id = ?";
         return jdbcTemplate.update(sql, persona.getNombre(), persona.getEmail(), persona.getId());
     }
